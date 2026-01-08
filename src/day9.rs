cabbf735ef80af93133e7dc6805c5e7cc3a931d9
Soham Nandy <soham.nandy2006@gmail.com>
2026-01-08 22:58:13 +0530

    optimized 9(1)

--- src/day9.rs
+++ src/day9.rs
@@ -1,12 +1,16 @@
+use std::ops::Add;
 use crate::utils::parser::coords_i64;
 
 #[aoc(day9, part1)]
 fn part1(input: &str) -> usize {
     let mut max_area = 0;
     let coords = coords_i64(input);
-    for (x,y) in &coords {
-        for (i,j) in &coords{
-            let area = ((x-i).abs() + 1)* ((y-j).abs() + 1);
+    for i in 0..coords.len() {
+        let (x1,y1) = coords[i];
+
+        for j in i+1..coords.len(){
+            let (x2,y2) = coords[j];
+            let area = (x1 - x2).abs().add(1) * (y1 - y2).abs().add(1);
             max_area = max_area.max(area);
         }
     }
